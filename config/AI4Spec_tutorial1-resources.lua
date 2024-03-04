39d214249b344149a0866472d299b91c3fa0c447
Laxmana Yetukuri <yetukuri@puhti-login11.bullx>
2024-03-04 22:22:49 +0200

    AI4Spec notebooks on Puhti

---
+++ config/AI4Spec_tutorial1-resources.lua
@@ -0,0 +1,5 @@
+cores: 2
+time: "01:00:00"
+partition: "interactive"
+local_disk: 32
+mem: "4GB"
---
+++ config/AI4Spec_tutorial2.lua
@@ -0,0 +1,11 @@
+-- Jupyter
+prepend_path("PATH", "/projappl/project_2001659/yetukuri/AI4Spec/bin")
+
+setenv("_COURSE_BASE_NAME", "AI4Spec")
+setenv("_COURSE_NOTEBOOK", "AI4Spec/AI4Spec_Tutorial2.ipynb")
+setenv("_COURSE_GIT_REPO", "https://github.com/yetulaxman/AI4Spec.git")
+setenv("_COURSE_GIT_REF", "")
+setenv("_COURSE_NOTEBOOK_TYPE", "notebook")
+
+-- Extra course material
+setenv("_COURSE_SRC", "")
